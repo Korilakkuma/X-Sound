9db7d44facc7ecfa0ded25903f91507f92225cb5
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-06-09 02:13:07 +0900

    Update Service Worker

--- javascripts/service-worker.js
+++ javascripts/service-worker.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    var CACHE_VERSION = '1.0.2';
+    var CACHE_VERSION = '1.0.3';
     var CACHE_NAME    = 'x-sound-cache-v' + CACHE_VERSION;
 
     var cacheFiles = [
--- register-service-worker.js
+++ register-service-worker.js
@@ -1,1 +1,1 @@
-!function(){navigator.serviceWorker&&navigator.serviceWorker.register("service-worker.js").then(function(r){})["catch"](function(r){})}();
+!function(){navigator.serviceWorker&&navigator.serviceWorker.register("service-worker.js").then(function(r){}).catch(function(r){})}();
