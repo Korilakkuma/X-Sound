9eb8d2ce1bd67f732436eefe3351e650097846ca
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-11-22 23:49:29 +0900

    Update cache version

--- javascripts/service-worker.js
+++ javascripts/service-worker.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    var CACHE_VERSION = '2.1.18';
+    var CACHE_VERSION = '2.1.19';
     var CACHE_NAME    = 'x-sound-cache-v' + CACHE_VERSION;
 
     var BASE_URL = '/X-Sound/';
