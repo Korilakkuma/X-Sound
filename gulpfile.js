07fc47e084216bfdf39563b4bd9f4ebe34df3057
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-12-24 22:32:22 +0900

    v0.10.0

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var sass       = require('gulp-sass');
 var rename     = require('gulp-rename');
 
 gulp.task('jshint', function() {
-    gulp.src(['javascripts/controller.js', 'javascripts/register-service-woker.js', 'javascripts/service-worker.js'])
+    return gulp.src(['javascripts/controller.js', 'javascripts/register-service-woker.js', 'javascripts/service-worker.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
@@ -21,14 +21,14 @@ gulp.task('build:js', function() {
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('javascripts/'));
 
-   gulp.src(['javascripts/register-service-worker.js', 'javascripts/service-worker.js'])
+   return gulp.src(['javascripts/register-service-worker.js', 'javascripts/service-worker.js'])
        .pipe(plumber())
        .pipe(uglify())
        .pipe(gulp.dest('./'));
 });
 
 gulp.task('build:css', function() {
-    gulp.src('stylesheets/scss/*.scss')
+    return gulp.src('stylesheets/scss/*.scss')
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass({
@@ -38,11 +38,11 @@ gulp.task('build:css', function() {
         .pipe(gulp.dest('stylesheets/css/'));
 });
 
-gulp.task('build', ['build:js', 'build:css']);
+gulp.task('build', gulp.series(gulp.parallel('build:js', 'build:css')));
 
 gulp.task('watch', function() {
-    gulp.watch('javascripts/controller.js',              ['build:js']);
-    gulp.watch('javascripts/register-service-worker.js', ['build:js']);
-    gulp.watch('javascripts/service-worker.js',          ['build:js']);
-    gulp.watch('stylesheets/scss/*.scss',                ['build:css']);
+    gulp.watch('javascripts/controller.js',              gulp.task('build:js'));
+    gulp.watch('javascripts/register-service-worker.js', gulp.task('build:js'));
+    gulp.watch('javascripts/service-worker.js',          gulp.task('build:js'));
+    gulp.watch('stylesheets/scss/*.scss',                gulp.task('build:css'));
 });
