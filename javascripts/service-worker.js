5636d329074b71391e83150bc477b6d96da55d11
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-09-04 20:00:00 +0900

    Update cache version

--- javascripts/service-worker.js
+++ javascripts/service-worker.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    var CACHE_VERSION = '2.1.49';
+    var CACHE_VERSION = '2.1.50';
     var CACHE_NAME    = 'x-sound-cache-v' + CACHE_VERSION;
 
     var BASE_URL = '/';
